30a2066d63043fe85c7e1617d4f690000bec7073
tiagolimar <tiagolima.r2@gmail.com>
2025-01-23 21:39:13 -0300

    criando do arquivo principal do projeto

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Fastify from 'fastify';
+import routes from './routes/routes.js';
 
 let fastify = Fastify({ logger: true });
 
@@ -6,4 +7,6 @@ fastify.get('/', (request, reply) => {
     return { message: 'Oi! chamado a partir da raiz!' }
 });
 
-fastify.listen({ host:'0.0.0.0', port: 3000 }, ()=>console.log('server running on port 3000'));
+fastify = routes(fastify);
+
+fastify.listen({ host:'node', port: 3000 }, ()=>console.log('server running on port 3000'));
