a2216c15eed149a5c756c3e7f5effaf513e37ebd
tiagolimar <tiagolima.r2@gmail.com>
2025-01-23 21:34:12 -0300

    criando conexao com db

---
+++ connector/connector.js
@@ -0,0 +1,12 @@
+import pg from 'pg';
+import 'dotenv/config';
+
+const { Pool } = pg
+ 
+export const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+})
